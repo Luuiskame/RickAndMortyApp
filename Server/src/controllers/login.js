846166891a19b0887861082a3172da6db7b626d0
Luis Reyes <yuiftw05@gmail.com>
2023-09-11 14:17:54 -0600

    fixing the login

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,25 +1,11 @@
 const users = require('../utils/users')
 
-// function login(req,res){
-//     const {email, password} = req.query
-//     const found = users.find(user=> user.email === email && user.password === password)
-
-//     const access = found ? true : false
-//     res.status(200).json({access})
-// }
-
-// module.exports = login
-
-
-
 function login(req,res){
     const {email, password} = req.query
+    const found = users.find(user=> user.email === email && user.password === password)
 
-    if(users === email && users.password === password){
-        res.status(200).json({access:true})
-    } else {
-        res.status(200).json({access:false})
-    }
+    const access = found ? true : false
+    res.status(200).json({access})
 }
 
 module.exports = login
