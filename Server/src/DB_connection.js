841c1959d707ab45e6c7e75b1560eac64949544b
Luis Reyes <yuiftw05@gmail.com>
2024-02-04 14:29:58 -0600

    feat: adding the url to the vercel database

--- Server/src/DB_connection.js
+++ Server/src/DB_connection.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
 const {Sequelize} = require('sequelize')
-const {DB_USER, DB_PASSWORD, DB_HOST} = process.env  //! enviorment 
+const {DB_USER, DB_PASSWORD, DB_HOST, POSTGRES_URL} = process.env  //! enviorment 
 const FavoriteModel = require('./models/Favorite')
 const UserModel = require('./models/User')
+const pg = require('pg')
 
 //sequelize instance
-const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
-    {logging: false, native:false}
-)
+const sequelize = new Sequelize(POSTGRES_URL + "?sslmode=require", {
+    dialectModule: pg,
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  });
 
 //executing models
 FavoriteModel(sequelize)
